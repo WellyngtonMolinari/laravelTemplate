58aaef183bf2c2c5952e75132aa982268a6f24c2
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-08-08 13:36:52 -0300

    blog filter per category

--- resources/views/frontend/cat_blog_details.blade.php
+++ resources/views/frontend/cat_blog_details.blade.php
@@ -1,196 +1,84 @@
 @extends('frontend.main_master')
 @section('main')
 
-<main>
+@section('title')
+Blog | Thais Raffaelli
+@endsection
 
-            <!-- breadcrumb-area -->
-            <section class="breadcrumb__wrap">
-                <div class="container custom-container">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-6 col-lg-8 col-md-10">
-                            <div class="breadcrumb__wrap__content">
-               <h2 class="title"> {{ $categoryname->blog_category }} </h2>
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">Blog</li>
-                                    </ol>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="breadcrumb__wrap__icon">
-                    <ul>
-                        <li><img src="assets/img/icons/breadcrumb_icon01.png" alt=""></li>
-                        <li><img src="assets/img/icons/breadcrumb_icon02.png" alt=""></li>
-                        <li><img src="assets/img/icons/breadcrumb_icon03.png" alt=""></li>
-                        <li><img src="assets/img/icons/breadcrumb_icon04.png" alt=""></li>
-                        <li><img src="assets/img/icons/breadcrumb_icon05.png" alt=""></li>
-                        <li><img src="assets/img/icons/breadcrumb_icon06.png" alt=""></li>
-                    </ul>
-                </div>
-            </section>
-            <!-- breadcrumb-area-end -->
+<main id="main">
 
-
-            <!-- blog-area -->
-            <section class="standard__blog">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8">
-
-                        @foreach($blogpost as $item)
-<div class="standard__blog__post">
-    <div class="standard__blog__thumb">
-        <a href="blog-details.html"><img src="{{ asset($item->blog_image) }}" alt=""></a>
-        <a href="blog-details.html" class="blog__link"><i class="far fa-long-arrow-right"></i></a>
-    </div>
-    <div class="standard__blog__content">
-        <div class="blog__post__avatar">
-            <div class="thumb"><img src="{{ asset($item->blog_image) }}" alt=""></div>
-            <span class="post__by">By : <a href="#">Halina Spond</a></span>
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+  
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Blog Details</li>
+          </ol>
+          <h2></h2>
+  
         </div>
-        <h2 class="title"><a href="{{ route('blog.details',$item->id) }}">{{$item->blog_title}}</a></h2>
-        <p>{!! Str::limit($item->blog_description, 200) !!}  </p>
-        <ul class="blog__post__meta">
-            <li><i class="fal fa-calendar-alt"></i> {{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</li>
-            
-            
-        </ul>
-    </div>
-</div>
-                            @endforeach
+      </section><!-- End Breadcrumbs -->
 
-
-                            <div class="pagination-wrap">
-                                <nav aria-label="Page navigation example">
-                                    <ul class="pagination">
-                                        <li class="page-item"><a class="page-link" href="#"><i class="far fa-long-arrow-left"></i></a></li>
-                                        
-                                        
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <aside class="blog__sidebar">
-                                <div class="widget">
-                                    <form action="#" class="search-form">
-                                        <input type="text" placeholder="Search">
-                                        <button type="submit"><i class="fal fa-search"></i></button>
-                                    </form>
-                                </div>
-
-
-        <div class="widget">
-            <h4 class="widget-title">Recent Blog</h4>
-            <ul class="rc__post">
-
-               @foreach($allblogs as $all )
-                <li class="rc__post__item">
-                    <div class="rc__post__thumb">
-                        <a href="blog-details.html"><img src="{{ asset($all->blog_image) }} " alt=""></a>
-                    </div>
-                    <div class="rc__post__content">
-                        <h5 class="title"><a href="blog-details.html">{{ $all->blog_title }}
-                         </a></h5>
-                        <span class="post-date"><i class="fal fa-calendar-alt"></i> {{ Carbon\Carbon::parse($all->created_at)->diffForHumans() }} </span>
-                    </div>
-                </li>
-                @endforeach
-                                        
-                                    </ul>
-                                </div>
-
-                               <div class="widget">
-            <h4 class="widget-title">Categories</h4>
-            <ul class="sidebar__cat">
-            	@foreach($categories as $cat)
-                <li class="sidebar__cat__item"><a href="{{ route('category.blog',$cat->id) }}">{{ $cat->blog_category  }}  </a></li>
-                @endforeach
-            </ul>
+    <!-- Blog Page Title & Breadcrumbs -->
+    <div data-aos="fade" class="page-title">
+      <div class="heading">
+        <div class="container">
+          <div class="row d-flex justify-content-center text-center">
+            <div class="col-lg-8">
+              <h1>Blog</h1>
+              <p class="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+            </div>
+          </div>
         </div>
+      </div>
+    </div><!-- End Page Title -->
 
+    <!-- Blog Section - Blog Page -->
+    <section id="blog" class="blog">
 
-                                <div class="widget">
-                                    <h4 class="widget-title">Recent Comment</h4>
-                                    <ul class="sidebar__comment">
-                                        <li class="sidebar__comment__item">
-                                            <a href="blog-details.html">Rasalina Sponde</a>
-                                            <p>There are many variations of passages of lorem ipsum available, but the majority have</p>
-                                        </li>
-                                        <li class="sidebar__comment__item">
-                                            <a href="blog-details.html">Rasalina Sponde</a>
-                                            <p>There are many variations of passages of lorem ipsum available, but the majority have</p>
-                                        </li>
-                                        <li class="sidebar__comment__item">
-                                            <a href="blog-details.html">Rasalina Sponde</a>
-                                            <p>There are many variations of passages of lorem ipsum available, but the majority have</p>
-                                        </li>
-                                        <li class="sidebar__comment__item">
-                                            <a href="blog-details.html">Rasalina Sponde</a>
-                                            <p>There are many variations of passages of lorem ipsum available, but the majority have</p>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="widget">
-                                    <h4 class="widget-title">Popular Tags</h4>
-                                    <ul class="sidebar__tags">
-                                        <li><a href="blog.html">Business</a></li>
-                                        <li><a href="blog.html">Design</a></li>
-                                        <li><a href="blog.html">apps</a></li>
-                                        <li><a href="blog.html">landing page</a></li>
-                                        <li><a href="blog.html">data</a></li>
-                                        <li><a href="blog.html">website</a></li>
-                                        <li><a href="blog.html">book</a></li>
-                                        <li><a href="blog.html">Design</a></li>
-                                        <li><a href="blog.html">product design</a></li>
-                                        <li><a href="blog.html">landing page</a></li>
-                                        <li><a href="blog.html">data</a></li>
-                                    </ul>
-                                </div>
-                            </aside>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- blog-area-end -->
+      <div class="container" data-aos="fade-up" data-aos-delay="100">
 
+        <div class="row gy-4 posts-list">
+            @foreach($blogpost as $item)
 
-            <!-- contact-area -->
-            <section class="homeContact homeContact__style__two">
-                <div class="container">
-                    <div class="homeContact__wrap">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="section__title">
-                                    <span class="sub-title">07 - Say hello</span>
-                                    <h2 class="title">Any questions? Feel free <br> to contact</h2>
-                                </div>
-                                <div class="homeContact__content">
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form</p>
-                                    <h2 class="mail"><a href="mailto:Info@webmail.com">Info@webmail.com</a></h2>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="homeContact__form">
-                                    <form action="#">
-                                        <input type="text" placeholder="Enter name*">
-                                        <input type="email" placeholder="Enter mail*">
-                                        <input type="number" placeholder="Enter number*">
-                                        <textarea name="message" placeholder="Enter Massage*"></textarea>
-                                        <button type="submit">Send Message</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- contact-area-end -->
+          <div class="col-xl-4 col-lg-6">
+            
+            <article>
 
-        </main>
+              <div class="post-img">
+                <img src="{{ asset($item->blog_image) }}" alt="" class="img-fluid">
+              </div>
+
+              <p class="post-category">{{$item->blog_tags}}</p>
+
+              <h2 class="title">
+                <a href="{{ route('blog.details',$item->id) }}">{{$item->blog_title}}</a>
+              </h2>
+
+              <p class="post-description">{!! Str::limit($item->blog_description, 200) !!} </p>
+                  <li><i class="fal fa-calendar-alt"></i> {{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</li>
+            </article>
+            
+          </div><!-- End post list item -->
+          @endforeach
+
+          
+          
+
+        </div><!-- End blog posts list -->
+
+        <div class="pagination d-flex justify-content-center">
+          <ul>
+            {{-- {{ $allblogs->links('vendor.pagination.custom') }} --}}
+          </ul>
+        </div><!-- End pagination -->
+
+      </div>
+
+    </section><!-- End Blog Section -->
+
+  </main>
 
 
 
